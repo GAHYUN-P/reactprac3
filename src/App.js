7157f02492246adf5dd31969ea76051cff81e2c3
GAHYUN-P <gahyun94526@gmail.com>
2022-01-28 22:56:47 +0900

    버킷리스트 뼈대잡기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// BucketList 컴포넌트를 import 해옵니다. import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from './BucketList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const list = ['영화관 가기', '매일 책읽기', '수영 배우기'];
+
+    return (
+        <div className="App">
+            <h1>내 버킷리스트</h1>
+            {/* 컴포넌트를 넣어줍니다. */}
+            <BucketList list_a = {list}/>
+        </div>
+    );
 }
 
 export default App;
